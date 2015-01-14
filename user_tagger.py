c595e70ac39834d5a637c296fcfdc46c114dca25
Eric Boersma <eric.boersma@gmail.com>
2015-01-14 11:39:42 -0500

    Read the CSV

--- user_tagger.py
+++ user_tagger.py
@@ -5,7 +5,20 @@ Usage:
  user_tagger.py <filename>
 """
 from docopt import docopt
+import csv
+import json
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     filename = arguments['<filename>']
+    with open(filename, 'r') as csvfile:
+        rows = csv.DictReader(csvfile)
+        for row in rows:
+            row_dict = {
+                'id': row['id'],
+                'name': row['name'],
+                'value': row['value']
+            }
+            row_json = json.dumps(row_dict)
+            
